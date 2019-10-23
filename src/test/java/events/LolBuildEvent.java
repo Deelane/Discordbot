f8b533291fa7cb95a524ca446ba2298f9636d298
Fooping <55857290+Fooping@users.noreply.github.com>
2019-10-23 14:53:15 -0700

    Added game mode selection functionality
    Added game mode selection functionality via a 3rd argument, needs to be tested first

--- src/test/java/events/LolBuildEvent.java
+++ src/test/java/events/LolBuildEvent.java
@@ -5,18 +5,48 @@ import java.util.List;
 
 public class LolBuildEvent implements MessageEvent
 {
-    private String link = "";
+    private String link = "https://www.metasrc.com/na/";
     private String champ = "";
+    private String mode = "";
+
     public void setParameters(List<String> Parsedmsg)
     {
         champ = Parsedmsg.get(1).toLowerCase().trim();
-        if (isChamp())
+        mode = Parsedmsg.get(2).toLowerCase().trim();
+        if (Parsedmsg.size() == 2 )
         {
-            link = "https://www.metasrc.com/5v5/champion/" + champ.replaceAll("[^A-Za-z]+", "");
+            if (isChamp())
+            {
+                link += "5v5/champion/" + champ.replaceAll("[^A-Za-z]+", "");
+            }
+            else
+            {
+                link = "Not a valid champion.";
+            }
+        }
+        else if (Parsedmsg.size() == 3)
+        {
+            if (isMode())
+            {
+                link += mode.replaceAll("[^A-Za-z]+", "") + "/";
+            }
+            else
+            {
+                link = "Not a valid mode.";
+            }
+            if (isChamp())
+            {
+                link += "champion/" + champ.replaceAll("[^A-Za-z]+", "");
+            }
+            else
+            {
+                link = "Not a valid champion.";
+            }
+
         }
         else
         {
-            link = "Not a valid champion.";
+            link = "Invalid syntax.\n" + "Proper syntax:\n" + "For default role: !lolbuild, champion\n" + "For specific mode: !lolbuild, champion, mode";
         }
     }
 
@@ -25,7 +55,7 @@ public class LolBuildEvent implements MessageEvent
         return link;
     }
 
-    public boolean isChamp()
+    private boolean isChamp()
     {
         try
         {
@@ -35,6 +65,30 @@ public class LolBuildEvent implements MessageEvent
             String str;
             while ((str = br.readLine()) != null) {
                 if (str.equalsIgnoreCase(champ)) {
+                    return true;
+                }
+            }
+        }
+        catch (FileNotFoundException ex)
+        {
+            ex.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    private boolean isMode()
+    {
+        try
+        {
+            String path = getClass().getClassLoader().getResource("LolGameModes.properties").getPath();
+            File LolGameModes = new File(path);
+            BufferedReader br = new BufferedReader(new FileReader(LolGameModes));
+            String str;
+            while ((str = br.readLine()) != null) {
+                if (str.equalsIgnoreCase(mode)) {
                     return true;
                 }
             }
